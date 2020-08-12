4dfe861aaf16c88732d7f313272cabc08fa4d004
GirishJayakumar <66281004+GirishJayakumar@users.noreply.github.com>
2020-08-12 11:40:57 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/servezy/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.servezy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button next;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        next = findViewById(R.id.welcomeButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignIn();
+            }
+        });
+    }
+    public void openSignIn(){
+        Intent openMain= new Intent(this, SignIn.class );
+        startActivity(openMain);
+    }
+}
---
+++ src/main/java/com/example/servezy/Calendar.java
@@ -0,0 +1,49 @@
+package com.example.servezy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class Calendar extends AppCompatActivity {
+    private static final String TAG = "CalendarActivity";
+    private CalendarView calender;
+    TextView date_view;
+    private Button signed_in;
+
+    public void backToSignIn(){
+        Intent backtoSignin= new Intent(this, SignIn.class );
+        startActivity(backtoSignin);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        signed_in = findViewById(R.id.backFromCalender);
+        signed_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToSignIn();
+            }
+        });
+
+        calender = (CalendarView) findViewById(R.id.calendarView);
+        date_view = (TextView) findViewById(R.id.date_view);
+
+        calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = year+"."+(month+1)+"."+dayOfMonth;
+                date_view.setText(date);
+            }
+        });
+
+
+    }
+}
